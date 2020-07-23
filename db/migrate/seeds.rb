c88ffce73c49ef68076d2da241221d76419fa8c3
AlveeM <alvee.akand@outlook.com>
2020-07-23 07:59:59 -0400

    Done.

---
+++ db/migrate/seeds.rb
@@ -0,0 +1,18 @@
+Song.destroy_all
+
+Song.create([
+  {
+    title: 'Rhythms Which I Played',
+    release_year: 2000,
+    released: true,
+    genre: 'Normcore',
+    artist_name: 'Man With Instruments'
+  },
+  {
+    title: 'Rhythms Which I Played Again at a Later Time',
+    release_year: 2004,
+    released: true,
+    genre: 'Sci-Crust Fizz-Punk',
+    artist_name: 'Man With Instruments'
+  }
+])
